41ac4dadbd6ccb42689052ac55816466a2ca14eb
Masum Billah <masumbillah2062003@gmail.com>
2025-09-10 03:18:39 +0600

    calculaton1

--- calculation1.js
+++ calculation1.js
@@ -1,3 +1,20 @@
+/* 
+    The Simple String Calculator
+    Your Goal: Write a program that can calculate the sum of numbers found inside a string.
+    How it Works
+    Your program will take a single string of text. Inside this string, there will be numbers separated by plus signs (+) and letters. You must find all the numbers and add them together.
+    Rules
+    Your program will read one string as input.
+    The program must only add together numbers.
+    Letters and other symbols should be ignored.
+
+    Input String-----Final Sum
+    10+20+30-------60
+    10+20a+30--------60
+    1+2+hello+3+4-----10
+*/
+
+
 let symbols = "!@#$%^&*()-_=+[]{}\\|;:'\",<.>/?`~";
 
 const sumFunction = (input) => {
